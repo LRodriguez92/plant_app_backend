48b8575d5430b9b00bbce6c76a5c3a5178511fc6
Leo <leonardo@Leo.home>
2020-11-13 12:26:42 -0500

    created PUT for PlantDetailView

--- app/views.py
+++ app/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.parsers import JSONParser
 from .models import Plant, PlantImage, User, Schedule
 from .serializers import PlantSerializer, ScheduleSerializer, PlantImageSerializer
 
 
 # Create your views here.
 class PlantView(APIView):
+    # Only needs POST
+
     def get(self, request):
         plants = Plant.objects.all()
         serializer = PlantSerializer(plants, many=True)
@@ -35,12 +38,28 @@ class PlantView(APIView):
 
 
 class PlantDetailView(APIView):
+    # Needs GET, PUT and DELETE
+
     # add pk as an argument to retrieve the pk in the parameter.
     def get(self, request, pk):
         plant = Plant.objects.get(id=pk)
         serializer = PlantSerializer(plant)
 
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        try:
+            plant = Plant.objects.get(id=pk)
+        except:
+            return Response({'message': 'The plant does not exist'})
+
+        new_data = JSONParser().parse(request)
+        serializer = PlantSerializer(plant, data=new_data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class PlantImageView(APIView):
