151a7bed93fe042ee94c4514d88e9b5bcbf39535
Leo <leonardo@Leo.home>
2020-11-14 21:20:17 -0500

    added auth permissions

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from django.conf.urls import url, include
 from rest_framework import routers
-# from .api import RegisterApi
 from .views import PlantView, PlantDetailView, ScheduleView, PlantImageView, PlantImageDetailView, UserView
 
 router = routers.DefaultRouter()
@@ -17,7 +16,6 @@ urlpatterns = [
     path('plants/<int:pk>/schedule/', ScheduleView.as_view()),
     path('', include(router.urls)),
     path('auth/', include('rest_auth.urls')),
-    # path('register/', RegisterApi.as_view()),
 ]
 
 # urlpatterns = format_suffix_patterns(urlpatterns)
--- app/views.py
+++ app/views.py
@@ -3,6 +3,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
 from rest_framework.parsers import JSONParser
+from rest_framework.permissions import AllowAny
+
+from .permissions import IsLoggedInUserOrAdmin, IsAdminUser
 from .models import Plant, PlantImage, User, Schedule
 from .serializers import PlantSerializer, ScheduleSerializer, PlantImageSerializer, UserSerializer
 
@@ -174,3 +177,13 @@ class ScheduleView(APIView):
 class UserView(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def get_permissions(self):
+        permission_classes = []
+        if self.action == 'create':
+            permission_classes = [AllowAny]
+        elif self.action == 'retrieve' or self.action == 'update' or self.action == 'partial_update':
+            permission_classes = [IsLoggedInUserOrAdmin]
+        elif self.action == 'list' or self.action == 'destroy':
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
