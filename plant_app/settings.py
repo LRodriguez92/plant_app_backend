f798ae47d55c895bf3d005749d16f8a1bf08f730
Leo <leonardo@Leo.home>
2020-12-18 10:25:32 -0500

    commented out django heroku

--- plant_app/settings.py
+++ plant_app/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
-import django_heroku
+# import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -160,5 +160,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Activate Django-Heroku.
-django_heroku.settings(config=locals(), staticfiles=False, logging=False)
 # django_heroku.settings(locals())
